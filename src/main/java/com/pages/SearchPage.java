c918a09f8d6023c50a3fc84174dd01068daca228
sureshseela123 <sureshseela123@gmail.com>
2020-09-16 12:58:09 +0530

    changes in search page

--- src/main/java/com/pages/SearchPage.java
+++ src/main/java/com/pages/SearchPage.java
@@ -5,5 +5,9 @@ public class SearchPage {
 	public void searchPage(){
 		System.out.println("This is only for Search page");
 	}
+	
+	public void searchTitle(){
+		System.out.println("This is only for Searching the page title");
+	}
 
 }

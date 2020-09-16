824b09bf5a1ef83874aaa83b67a492e12d3ecaf0
sureshseela123 <sureshseela123@gmail.com>
2020-09-16 16:51:56 +0530

    added constructor in homepage

--- src/main/java/com/pages/Homepage.java
+++ src/main/java/com/pages/Homepage.java
@@ -2,6 +2,11 @@ package com.pages;
 
 public class Homepage {
 	
+	
+	public Homepage(){
+		System.out.println("constructor in homepage");
+	}
+	
 	public void homePage(){
 		System.out.println("This is only for Home page");
 	}

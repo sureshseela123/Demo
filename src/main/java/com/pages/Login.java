8d71811bd33d9b72d36f24f0f34b099d92e08c01
sureshseela123 <sureshseela123@gmail.com>
2020-09-16 17:05:56 +0530

    added constructor in login page

--- src/main/java/com/pages/Login.java
+++ src/main/java/com/pages/Login.java
@@ -2,6 +2,10 @@ package com.pages;
 
 public class Login {
 	
+	public Login(){
+		System.out.println("constructor in the Login page");
+	}
+	
 	public void loginTest()
 	{
 		System.out.println("This class is for login page");
